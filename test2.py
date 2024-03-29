d4e6506f0fead5d9cb355b6136fc2623c5a46d74
Yoonsangbyung <ysb1706@naver.com>
2024-03-29 16:46:44 +0900

    4주차

---
+++ test2.py
@@ -0,0 +1,39 @@
+r = 10
+t = 1.5
+
+print(r*t)
+
+print('엄마가 말했다. "밥 먹고 가"')
+
+print(" 저의 이름은 %s입니다." % "홍길동")
+
+a = "Life is too short, You need python"
+print(a[0], a[-1])
+
+date = "2020.10.23"
+date.replace(".", "-")
+print(date)
+
+print(3 + 2 * 4)
+
+fruits = ["사과", "바나나", "딸기"]
+
+print(fruits, type(fruits))
+
+b = list(range(3, 12, 2))
+print(b)
+
+a = [1,2,3,4,5]
+a.append(10)
+a.remove(3)
+print(a)
+
+person = {"이름": "윤상병", "나이": 24}
+print(person["이름"])
+
+person = {"이름": "윤상병", "나이": 24}
+print(person.keys())
+
+person = {"이름": "윤상병", "나이": 24}
+print(person.values())
+
